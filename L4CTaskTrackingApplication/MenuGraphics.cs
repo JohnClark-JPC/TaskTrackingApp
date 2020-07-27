26e7d46157fb1ab780af6b20da25d5aac2491d6e
John Clark <john.clark012@outlook.com>
2020-07-27 12:28:50 -0400

    Added limitedMenu for when list is empty

--- L4CTaskTrackingApplication/MenuGraphics.cs
+++ L4CTaskTrackingApplication/MenuGraphics.cs
@@ -27,6 +27,24 @@ namespace L4CTaskTrackingApplication
             Console.ForegroundColor = ConsoleColor.White;
         }
 
+        public static void limitedMenuHeader()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(@"
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                           //
+//                                       ARMAGEDDON IT!                                      //
+//                                                                                           //
+//                            1. Add new task                                                //
+//                            2. Exit                                                        //
+//                                                                                           //
+///////////////////////////////////////////////////////////////////////////////////////////////");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
         public static void subMenuHeader()
         {
             Console.BackgroundColor = ConsoleColor.DarkBlue;
